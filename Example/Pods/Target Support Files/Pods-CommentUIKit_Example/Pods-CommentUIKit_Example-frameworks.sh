22352491c9fae1dee770fb595ffc0b6704760d1c
tangquan <tomboyt@163.com>
2022-06-10 21:36:35 +0800

    init kit

--- Example/Pods/Target Support Files/Pods-CommentUIKit_Example/Pods-CommentUIKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-CommentUIKit_Example/Pods-CommentUIKit_Example-frameworks.sh
@@ -176,10 +176,10 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/CommentUIKit/CommentUIKit.framework"
+  install_framework "${PODS_ROOT}/../../CommentUIKit/Vendors/CommentUIKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/CommentUIKit/CommentUIKit.framework"
+  install_framework "${PODS_ROOT}/../../CommentUIKit/Vendors/CommentUIKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
